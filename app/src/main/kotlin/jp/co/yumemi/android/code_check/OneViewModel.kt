2ee02ae699d46534d8253db29d6545a1a1abf799
negitorogithub <s_tsuruta@toridori.co.jp>
2025-09-07 11:20:35 +0900

    エラーハンドリングを追加

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/OneViewModel.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/OneViewModel.kt
@@ -29,52 +29,56 @@ class OneViewModel() : ViewModel() {
     fun searchResults(inputText: String): List<Item> = runBlocking {
         val client = HttpClient(Android)
         return@runBlocking GlobalScope.async {
-            val response: HttpResponse = client.get("https://api.github.com/search/repositories") {
-                header("Accept", "application/vnd.github.v3+json")
-                parameter("q", inputText)
-            }
+            try {
+                val response: HttpResponse =
+                    client.get("https://api.github.com/search/repositories") {
+                        header("Accept", "application/vnd.github.v3+json")
+                        parameter("q", inputText)
+                    }
 
-            if (!response.status.isSuccess()) {
-                Log.e("Search Repositories Error", response.status.toString())
-                return@async emptyList()
-            }
+                if (!response.status.isSuccess()) {
+                    Log.e("Search Repositories Error", response.status.toString())
+                    return@async emptyList()
+                }
 
 
-            val jsonBody = JSONObject(response.receive<String>())
+                val jsonBody = JSONObject(response.receive<String>())
+                val jsonItems = jsonBody.optJSONArray("items")!!
+                val items = mutableListOf<Item>()
 
-            val jsonItems = jsonBody.optJSONArray("items")!!
+                /**
+                 * アイテムの個数分ループする
+                 */
+                for (i in 0 until jsonItems.length()) {
+                    val jsonItem = jsonItems.optJSONObject(i)!!
+                    val name = jsonItem.optString("full_name")
+                    val ownerIconUrl = jsonItem.optJSONObject("owner")!!.optString("avatar_url")
+                    val language = jsonItem.optString("language")
+                    val stargazersCount = jsonItem.optLong("stargazers_count")
+                    val watchersCount = jsonItem.optLong("watchers_count")
+                    val forksCount = jsonItem.optLong("forks_count")
+                    val openIssuesCount = jsonItem.optLong("open_issues_count")
 
-            val items = mutableListOf<Item>()
-
-            /**
-             * アイテムの個数分ループする
-             */
-            for (i in 0 until jsonItems.length()) {
-                val jsonItem = jsonItems.optJSONObject(i)!!
-                val name = jsonItem.optString("full_name")
-                val ownerIconUrl = jsonItem.optJSONObject("owner")!!.optString("avatar_url")
-                val language = jsonItem.optString("language")
-                val stargazersCount = jsonItem.optLong("stargazers_count")
-                val watchersCount = jsonItem.optLong("watchers_count")
-                val forksCount = jsonItem.optLong("forks_count")
-                val openIssuesCount = jsonItem.optLong("open_issues_count")
-
-                items.add(
-                    Item(
-                        name = name,
-                        ownerIconUrl = ownerIconUrl,
-                        language = language,
-                        stargazersCount = stargazersCount,
-                        watchersCount = watchersCount,
-                        forksCount = forksCount,
-                        openIssuesCount = openIssuesCount
+                    items.add(
+                        Item(
+                            name = name,
+                            ownerIconUrl = ownerIconUrl,
+                            language = language,
+                            stargazersCount = stargazersCount,
+                            watchersCount = watchersCount,
+                            forksCount = forksCount,
+                            openIssuesCount = openIssuesCount
+                        )
                     )
-                )
+                }
+                lastSearchDate = Date()
+                return@async items.toList()
+            } catch (e: Exception) {
+                Log.e("Search Repositories Error", e.toString())
+                throw e
+            } finally {
+                client.close()
             }
-
-            lastSearchDate = Date()
-
-            return@async items.toList()
         }.await()
     }
 }
