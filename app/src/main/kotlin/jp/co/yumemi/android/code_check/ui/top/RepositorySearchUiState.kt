e45c9a5e225a92b64a0935a2efb5eaca8ed288c0
negitorogithub <s_tsuruta@toridori.co.jp>
2025-09-07 21:22:34 +0900

    RepositorySearchUiStateを追加

---
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/ui/top/RepositorySearchUiState.kt
@@ -0,0 +1,9 @@
+package jp.co.yumemi.android.code_check.ui.top
+
+import jp.co.yumemi.android.code_check.model.Item
+
+sealed class RepositorySearchUiState {
+    object Loading : RepositorySearchUiState()
+    object Error : RepositorySearchUiState()
+    data class Success(val repositories: List<Item>) : RepositorySearchUiState()
+}
