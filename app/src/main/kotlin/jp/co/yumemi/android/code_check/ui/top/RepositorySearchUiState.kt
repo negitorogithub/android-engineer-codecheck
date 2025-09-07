d02d0231f2e035999555b122a34f16f005b76a87
negitorogithub <s_tsuruta@toridori.co.jp>
2025-09-07 21:25:12 +0900

    検索画面の初期状態として `WaitingInput` を追加

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/ui/top/RepositorySearchUiState.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/ui/top/RepositorySearchUiState.kt
@@ -3,6 +3,7 @@ package jp.co.yumemi.android.code_check.ui.top
 import jp.co.yumemi.android.code_check.model.Item
 
 sealed class RepositorySearchUiState {
+    object WaitingInput : RepositorySearchUiState()
     object Loading : RepositorySearchUiState()
     object Error : RepositorySearchUiState()
     data class Success(val repositories: List<Item>) : RepositorySearchUiState()
