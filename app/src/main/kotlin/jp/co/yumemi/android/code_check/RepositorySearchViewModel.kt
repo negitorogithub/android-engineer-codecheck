a0edab0540aa63de008e351943f953d7a87dad9c
negitorogithub <s_tsuruta@toridori.co.jp>
2025-09-07 20:51:39 +0900

    viewModelScope.launch を使用してコルーチン内で検索処理を実行するように修正

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/RepositorySearchViewModel.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/RepositorySearchViewModel.kt
@@ -20,7 +20,7 @@ import kotlinx.coroutines.async
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.update
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.launch
 import org.json.JSONObject
 import java.util.Date
 
@@ -33,65 +33,66 @@ class RepositorySearchViewModel() : ViewModel() {
     val searchResultsState: StateFlow<List<Item>> = _searchResultsState
 
     // 検索結果
-    fun searchResults(inputText: String): Unit = runBlocking {
+    fun searchResults(inputText: String) {
         val client = HttpClient(Android)
+        viewModelScope.launch {
+            val result = viewModelScope.async {
+                try {
+                    val response: HttpResponse =
+                        client.get("https://api.github.com/search/repositories") {
+                            header("Accept", "application/vnd.github.v3+json")
+                            parameter("q", inputText)
+                        }
 
-        val result = viewModelScope.async {
-            try {
-                val response: HttpResponse =
-                    client.get("https://api.github.com/search/repositories") {
-                        header("Accept", "application/vnd.github.v3+json")
-                        parameter("q", inputText)
+                    if (!response.status.isSuccess()) {
+                        Log.e("Search Repositories Error", response.status.toString())
+                        return@async emptyList()
                     }
 
-                if (!response.status.isSuccess()) {
-                    Log.e("Search Repositories Error", response.status.toString())
-                    return@async emptyList()
-                }
+
+                    val jsonBody = JSONObject(response.receive<String>())
+                    val jsonItems = jsonBody.optJSONArray("items")
+                    if (jsonItems == null) throw Exception("jsonItems is null")
+                    val items = mutableListOf<Item>()
 
 
-                val jsonBody = JSONObject(response.receive<String>())
-                val jsonItems = jsonBody.optJSONArray("items")
-                if (jsonItems == null) throw Exception("jsonItems is null")
-                val items = mutableListOf<Item>()
+                    /**
+                     * アイテムの個数分ループする
+                     */
+                    for (i in 0 until jsonItems.length()) {
+                        val jsonItem = jsonItems.optJSONObject(i)
+                        val name = jsonItem.optString("full_name")
+                        val ownerOptJSONObject = jsonItem.optJSONObject("owner")
+                        if (ownerOptJSONObject == null) throw Exception("ownerOptJSONObject is null")
+                        val ownerIconUrl = ownerOptJSONObject.optString("avatar_url")
+                        val language = jsonItem.optString("language")
+                        val stargazersCount = jsonItem.optLong("stargazers_count")
+                        val watchersCount = jsonItem.optLong("watchers_count")
+                        val forksCount = jsonItem.optLong("forks_conut")
+                        val openIssuesCount = jsonItem.optLong("open_issues_count")
 
-
-                /**
-                 * アイテムの個数分ループする
-                 */
-                for (i in 0 until jsonItems.length()) {
-                    val jsonItem = jsonItems.optJSONObject(i)
-                    val name = jsonItem.optString("full_name")
-                    val ownerOptJSONObject = jsonItem.optJSONObject("owner")
-                    if (ownerOptJSONObject == null) throw Exception("ownerOptJSONObject is null")
-                    val ownerIconUrl = ownerOptJSONObject.optString("avatar_url")
-                    val language = jsonItem.optString("language")
-                    val stargazersCount = jsonItem.optLong("stargazers_count")
-                    val watchersCount = jsonItem.optLong("watchers_count")
-                    val forksCount = jsonItem.optLong("forks_conut")
-                    val openIssuesCount = jsonItem.optLong("open_issues_count")
-
-                    items.add(
-                        Item(
-                            name = name,
-                            ownerIconUrl = ownerIconUrl,
-                            language = language,
-                            stargazersCount = stargazersCount,
-                            watchersCount = watchersCount,
-                            forksCount = forksCount,
-                            openIssuesCount = openIssuesCount
+                        items.add(
+                            Item(
+                                name = name,
+                                ownerIconUrl = ownerIconUrl,
+                                language = language,
+                                stargazersCount = stargazersCount,
+                                watchersCount = watchersCount,
+                                forksCount = forksCount,
+                                openIssuesCount = openIssuesCount
+                            )
                         )
-                    )
+                    }
+                    lastSearchDate = Date()
+                    return@async items.toList()
+                } catch (e: Exception) {
+                    Log.e("Search Repositories Error", e.toString())
+                    throw e
+                } finally {
+                    client.close()
                 }
-                lastSearchDate = Date()
-                return@async items.toList()
-            } catch (e: Exception) {
-                Log.e("Search Repositories Error", e.toString())
-                throw e
-            } finally {
-                client.close()
-            }
-        }.await()
-        _searchResultsState.update { result }
+            }.await()
+            _searchResultsState.update { result }
+        }
     }
 }
