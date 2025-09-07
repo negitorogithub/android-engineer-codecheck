cd27e3ba2894683cb8a6dae035714abeacf0c43b
negitorogithub <s_tsuruta@toridori.co.jp>
2025-09-07 19:32:11 +0900

    gotoRepositoryFragmentを追加

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/ui/top/RepositorySearchFragment.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/ui/top/RepositorySearchFragment.kt
@@ -7,7 +7,11 @@ import android.view.ViewGroup
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import jp.co.yumemi.android.code_check.OneFragmentDirections
+import jp.co.yumemi.android.code_check.OneViewModel
 import jp.co.yumemi.android.code_check.R
+import jp.co.yumemi.android.code_check.model.Item
 import jp.co.yumemi.android.code_check.ui.theme.CodeCheckTheme
 
 class RepositorySearchFragment: Fragment(R.layout.fragment_one) {
@@ -16,16 +20,23 @@ class RepositorySearchFragment: Fragment(R.layout.fragment_one) {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+        val viewModel = OneViewModel()
+        fun gotoRepositoryFragment(item: Item) {
+            val action = OneFragmentDirections
+                .actionRepositoriesFragmentToRepositoryFragment(item = item)
+            findNavController().navigate(action)
+        }
         return ComposeView(requireContext()).apply {
             setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
             setContent {
                 CodeCheckTheme {
                     RepositorySearchScreen(
-                        onRepositoryClick = {  },
-                        search = {emptyList()}
+                        onRepositoryClick = { gotoRepositoryFragment(it) },
+                        search = viewModel::searchResults
                     )
                 }
             }
         }
     }
 }
+
