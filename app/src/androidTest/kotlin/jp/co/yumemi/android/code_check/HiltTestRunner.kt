dd064d80bf261f61650b89df0a63173e08b40899
negitorogithub <s_tsuruta@toridori.co.jp>
2025-09-14 16:59:11 +0900

    UIテストを実行するための依存関係を追加

---
+++ app/src/androidTest/kotlin/jp/co/yumemi/android/code_check/HiltTestRunner.kt
@@ -0,0 +1,16 @@
+package jp.co.yumemi.android.code_check
+
+import android.app.Application
+import android.content.Context
+import androidx.test.runner.AndroidJUnitRunner
+import dagger.hilt.android.testing.HiltTestApplication
+
+/**
+ * Hiltテスト用のカスタムテストランナー
+ */
+class HiltTestRunner : AndroidJUnitRunner() {
+
+    override fun newApplication(cl: ClassLoader?, name: String?, context: Context?): Application {
+        return super.newApplication(cl, HiltTestApplication::class.java.name, context)
+    }
+}
